096d3ad10464bf4b082d83c242abd7b6e960ca5d
hwalang <smdong97@gmail.com>
2024-11-21 16:43:39 +0900

    Feat( entry point ): test entry point
    main()을 entry point로 세팅했다.
    windows desktop application에서 제공했던 코드는 전부 삭제했다.

--- DX3D11/PT/pch.h
+++ DX3D11/PT/pch.h
@@ -1,9 +1,13 @@
 #pragma once
 
-// 자주 사용하는 standard library header
+// user-defined
+
+
+// std
 #include <iostream>
 #include <vector>
 #include <string>
 #include <map>
 
-// project header
+// windows
+#include <windows.h>
