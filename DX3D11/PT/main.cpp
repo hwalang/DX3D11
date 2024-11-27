9466c9882aea2468a3e9a410687e538eae32ace0
hwalang <smdong97@gmail.com>
2024-11-27 20:33:05 +0900

    refactor(main): clarify window instance creation
    window instance 생성 과정을 더 명확하게 이해할 수 있도록 수정했다.

--- DX3D11/PT/main.cpp
+++ DX3D11/PT/main.cpp
@@ -31,39 +31,49 @@ LRESULT CALLBACK WndProc ( HWND hwnd , UINT msg , WPARAM wParam , LPARAM lParam 
 
 int main ()
 {
-	// Create Window Instance
-	WNDCLASSEX wc = {
-		sizeof ( WNDCLASSEX ),			// cbSize: WNDCLASSEX structure의 크기
-		CS_CLASSDC,									// style: Window Class Style
-		WndProc,										// lpfnWndProc: Window Processor Function Pointer
-		0L,
-		0L,
-		GetModuleHandle(NULL),
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		L"PT",
-		NULL
-	};
+	WNDCLASSEX wc;
+	ZeroMemory ( &wc , sizeof ( WNDCLASSEX ) );
 
-	// Register Window Instance( Window 객체 등록 )
+	wc.cbSize = sizeof ( WNDCLASSEX );
+	wc.style = CS_CLASSDC;
+	wc.lpfnWndProc = WndProc;
+	wc.hCursor = LoadCursor ( NULL , IDC_ARROW );
+	wc.hbrBackground = ( HBRUSH ) COLOR_WINDOW;
+	wc.lpszClassName = L"WindowClass1";
+
 	if ( !RegisterClassEx ( &wc ) ) {
 		std::cout << "RegisterClassEx() failed." << std::endl;
 		return -1;
 	}
 
+	HWND mainWindow = CreateWindowEx ( 
+		NULL ,
+		wc.lpszClassName ,     // name of the window class
+		L"window1 title" ,     // title of the window
+		WS_OVERLAPPEDWINDOW ,  // window style
+		0 ,                    // x-position of the window
+		0 ,                    // y-position of the window
+		1280 ,                 // width of the window
+		960 ,                  // height of the window
+		NULL ,                 // we have no parent window
+		NULL ,                 // we aren't using menus
+		wc.hInstance ,         // apllication handle
+		NULL									 // used with multiple windows
+	);
+
+	/*
 	int screenWidth = 1280;
 	int screenHeight = 960;
-	RECT wr = { 0, 0, screenWidth, screenHeight };
+	RECT wr = {0, 0, screenWidth, screenHeight};
 	AdjustWindowRect ( &wr , WS_OVERLAPPEDWINDOW , false );
 
 	HWND mainWindow = CreateWindow(wc.lpszClassName, L"PT WND", WS_OVERLAPPEDWINDOW,
-																100, 
-																100,
+																0,
+																0,
 																wr.right - wr.left,
 																wr.bottom - wr.top,
 																NULL , NULL , wc.hInstance , NULL );
+	*/
 	if ( !mainWindow ) {
 		std::cout << "CreateWindow() failed." << std::endl;
 		return -1;
@@ -72,9 +82,8 @@ int main ()
 	ShowWindow ( mainWindow , SW_SHOWDEFAULT );
 	UpdateWindow ( mainWindow );
 
-	// main message loop
 	MSG msg = { 0 };
-	while ( WM_QUIT != msg.message ) {
+	while ( WM_QUIT != msg.message ) {	// GetMessage(&msg, NULL, 0, 0 )
 		if ( PeekMessage ( &msg , NULL , 0 , 0 , PM_REMOVE ) ) {
 			TranslateMessage ( &msg );
 			DispatchMessage ( &msg );
