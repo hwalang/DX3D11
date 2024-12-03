3057e9e95f99ba23d7ffdce6e5e18ceca8a9a7da
hwalang <smdong97@gmail.com>
2024-12-03 18:23:56 +0900

    feat(main): Initialize Direct3D

--- DX3D11/PT/main.cpp
+++ DX3D11/PT/main.cpp
@@ -1,33 +1,14 @@
 #include "pch.h"
 
-LRESULT CALLBACK WndProc ( HWND hwnd , UINT msg , WPARAM wParam , LPARAM lParam ) {
-	switch ( msg ) {
-	case WM_SIZE:
-		// Reset and resize swapchain
-		break;
-	case WM_SYSCOMMAND:
-		if ( ( wParam & 0xfff0 ) == SC_KEYMENU ) // Disable ALT application menu
-			return 0;
-		break;
-	case WM_MOUSEMOVE:
-		// cout << "Mouse " << LOWORD(lParam) << " " << HIWORD(lParam) << endl;
-		break;
-	case WM_LBUTTONUP:
-		// cout << "WM_LBUTTONUP Left mouse button" << endl;
-		break;
-	case WM_RBUTTONUP:
-		// cout << "WM_RBUTTONUP Right mouse button" << endl;
-		break;
-	case WM_KEYDOWN:
-		// cout << "WM_KEYDOWN " << (int)wParam << endl;
-		break;
-	case WM_DESTROY:
-		::PostQuitMessage ( 0 );
-		return 0;
-	}
+#pragma comment (lib, "d3d11.lib")
 
-	return DefWindowProc ( hwnd , msg , wParam , lParam );
-}
+IDXGISwapChain* swapChain;      // the pointer to the swap chain interface
+ID3D11Device* dev;              // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;    // the pointer to our Direct3D device context
+
+bool InitD3D ( HWND hWnd );
+void CleanD3D ( void );
+LRESULT CALLBACK WndProc ( HWND hwnd , UINT msg , WPARAM wParam , LPARAM lParam );
 
 int main ()
 {
@@ -74,6 +55,11 @@ int main ()
 	ShowWindow ( mainWindow , SW_SHOWDEFAULT );
 	UpdateWindow ( mainWindow );
 
+	if ( !InitD3D ( mainWindow ) ) {
+		std::cout << "InitD3D() failed." << std::endl;
+		return -1;
+	}
+
 	MSG msg = { 0 };
 	// check to see if it is time to quit
 	while ( WM_QUIT != msg.message ) {
@@ -87,5 +73,72 @@ int main ()
 		}
 	}
 
+	CleanD3D ();
+
 	return 0;
+}
+
+bool InitD3D ( HWND hWnd ) {
+
+	DXGI_SWAP_CHAIN_DESC scd;
+	ZeroMemory ( &scd , sizeof ( scd ) );
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 4;
+	scd.Windowed = TRUE;
+
+	if ( FAILED ( D3D11CreateDeviceAndSwapChain ( NULL ,
+		D3D_DRIVER_TYPE_HARDWARE ,
+		NULL ,
+		NULL ,
+		NULL ,
+		NULL ,
+		D3D11_SDK_VERSION ,
+		&scd ,
+		&swapChain ,
+		&dev ,
+		NULL ,
+		&devcon ) ) ) {
+		std::cout << "D3D11CreateDeviceAndSwapChain() failed." << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void CleanD3D () {
+	swapChain->Release ();
+	dev->Release ();
+	devcon->Release ();
+}
+
+LRESULT CALLBACK WndProc ( HWND hwnd , UINT msg , WPARAM wParam , LPARAM lParam ) {
+	switch ( msg ) {
+	case WM_SIZE:
+		// Reset and resize swapchain
+		break;
+	case WM_SYSCOMMAND:
+		if ( ( wParam & 0xfff0 ) == SC_KEYMENU ) // Disable ALT application menu
+			return 0;
+		break;
+	case WM_MOUSEMOVE:
+		// cout << "Mouse " << LOWORD(lParam) << " " << HIWORD(lParam) << endl;
+		break;
+	case WM_LBUTTONUP:
+		// cout << "WM_LBUTTONUP Left mouse button" << endl;
+		break;
+	case WM_RBUTTONUP:
+		// cout << "WM_RBUTTONUP Right mouse button" << endl;
+		break;
+	case WM_KEYDOWN:
+		// cout << "WM_KEYDOWN " << (int)wParam << endl;
+		break;
+	case WM_DESTROY:
+		::PostQuitMessage ( 0 );
+		return 0;
+	}
+
+	return DefWindowProc ( hwnd , msg , wParam , lParam );
 }
